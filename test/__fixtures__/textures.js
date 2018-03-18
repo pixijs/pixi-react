658ceff4a76d58ca0e076e253fcdb03a312349e0
Patrick Brouwer <patrick@inlet.nl>
2018-03-18 14:45:38 +0100

    Add test fixtures

---
+++ test/__fixtures__/textures.js
@@ -0,0 +1,5 @@
+import * as PIXI from 'pixi.js'
+
+export const emptyTexture = PIXI.Texture.fromImage(
+  'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABAQAAAAA3bvkkAAAAAnRSTlMAAHaTzTgAAAAKSURBVHgBY2AAAAACAAFzdQEYAAAAAElFTkSuQmCC'
+)

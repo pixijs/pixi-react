c5b892a2398a1c699f92aea2bf40d8bd91c1fb79
Javier Gonzalez <xaviergonz@gmail.com>
2018-05-27 21:05:22 +0200

    more modern omit

--- index.d.ts
+++ index.d.ts
@@ -2,9 +2,7 @@ import * as PIXI from 'pixi.js'
 import * as React from 'react'
 
 declare namespace _ReactPixi {
-  type Diff<T extends string, U extends string> = ({ [P in T]: P } & { [P in U]: never } & { [x: string]: never })[T]
-
-  type Omit<T, K extends keyof T> = Pick<T, Diff<keyof T, K>>
+  type Omit<T, K> = Pick<T, Exclude<keyof T, K>>
 
   interface ObjectWithChildren {
     children?: any

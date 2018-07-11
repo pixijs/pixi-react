75548bdabd72544237f3432d89e48fc075dc82de
Patrick Brouwer <patrick@inlet.nl>
2018-07-11 19:56:40 +0200

    Generate docs to `docs/`

--- .docz/app/root.jsx
+++ .docz/app/root.jsx
@@ -2,33 +2,16 @@ import React from 'react'
 import { hot } from 'react-hot-loader'
 import Theme from 'docz-theme-default'
 
-const socket = new WebSocket(`ws://127.0.0.1:8089`)
+import config from './config.json'
+import entries from './entries.json'
 
-class Root extends React.Component {
-  state = {
-    config: {},
-    entries: {},
-  }
-
-  async componentDidMount() {
-    socket.onmessage = ev => {
-      const message = JSON.parse(ev.data)
-
-      if (message.type === 'docz.entries') {
-        this.setState({ entries: message.data })
-      }
-
-      if (message.type === 'docz.config') {
-        this.setState({ config: message.data })
-      }
-    }
-  }
-
-  render() {
-    const { imports } = this.props
-
-    return <Theme {...this.state} imports={imports} hashRouter={false} />
-  }
-}
+const Root = ({ imports }) => (
+  <Theme
+    config={config}
+    entries={entries}
+    imports={imports}
+    hashRouter={true}
+  />
+)
 
 export default hot(module)(Root)
--- doczrc.js
+++ doczrc.js
@@ -2,6 +2,9 @@ export default {
   title: 'React Pixi',
   description: 'Write PIXI applications using React declarative style',
   propsParser: false,
+  ordering: 'ascending',
+  hashRouter: true,
+  dest: 'docs',
   themeConfig: {
     colors: {
       primary: 'tomato',

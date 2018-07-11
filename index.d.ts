2ce938a69cce8d41bf75928af1ad7391a90af49f
Patrick Brouwer <patrick@inlet.nl>
2018-07-11 20:58:49 +0200

    First argument of `render()` expects a React.Component instead of a PIXI instance

--- index.d.ts
+++ index.d.ts
@@ -43,7 +43,7 @@ declare namespace ReactPixi {
   const Stage: React.ComponentType<StageProps>
 
   function render(
-    pixiElement: PIXI.DisplayObject | PIXI.DisplayObject[],
+    reactPixiElement: React.ComponentType<any> | React.ComponentType<any>[],
     pixiContainer: PIXI.Container,
     callback?: Function
   ): void

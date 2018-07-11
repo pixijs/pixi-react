d05841f99773d85e12241422fb457885912bf9b6
Patrick Brouwer <patrick@inlet.nl>
2018-07-11 18:34:42 +0200

    Add title and disable PropsTables.

--- doczrc.js
+++ doczrc.js
@@ -1,5 +1,7 @@
 export default {
+  title: 'React Pixi',
   description: 'Write PIXI applications using React declarative style',
+  propsParser: false,
   themeConfig: {
     colors: {
       primary: 'tomato',

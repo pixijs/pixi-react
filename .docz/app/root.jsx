5184b7ae2e9fa5e2054853e9b8c6e286c71e861c
Patrick Brouwer <patrick@inlet.nl>
2018-09-10 11:57:10 +0200

    Build docz

--- .docz/app/root.jsx
+++ .docz/app/root.jsx
@@ -4,7 +4,12 @@ import Theme from 'docz-theme-default'
 import db from './db.json'
 
 const Root = ({ imports }) => (
-  <Theme db={db} imports={imports} hashRouter={true} />
+  <Theme
+    db={db}
+    imports={imports}
+    hashRouter={true}
+    websocketUrl="ws://127.0.0.1:60505"
+  />
 )
 
 export default hot(module)(Root)

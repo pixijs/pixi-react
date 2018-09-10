182d7d9974d0e0589668c976e1b241a18c56ce1a
Patrick Brouwer <patrick@inlet.nl>
2018-09-10 12:01:36 +0200

    Build docz

--- .docz/app/index.jsx
+++ .docz/app/index.jsx
@@ -1,4 +1,3 @@
-import 'webpack-serve-overlay'
 import React from 'react'
 import ReactDOM from 'react-dom'
 
--- .docz/app/root.jsx
+++ .docz/app/root.jsx
@@ -4,12 +4,7 @@ import Theme from 'docz-theme-default'
 import db from './db.json'
 
 const Root = ({ imports }) => (
-  <Theme
-    db={db}
-    imports={imports}
-    hashRouter={true}
-    websocketUrl="ws://127.0.0.1:60505"
-  />
+  <Theme db={db} imports={imports} hashRouter={true} />
 )
 
 export default hot(module)(Root)

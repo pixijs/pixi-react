fb7c80b93b082b3af41105518716ae64db5f2e84
Patrick Brouwer <patrick@inlet.nl>
2018-09-11 15:55:23 +0200

    Build docz

--- .docz/app/index.jsx
+++ .docz/app/index.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
-import { imports } from './imports'
 import Root from './root'
 
 const _onPreRenders = []
@@ -13,11 +11,7 @@ const onPostRender = () => _onPostRenders.forEach(f => f && f())
 const root = document.querySelector('#root')
 const render = (Component = Root) => {
   onPreRender()
-  ReactDOM.render(<Component imports={imports} />, root, onPostRender)
-}
-
-if (module.hot) {
-  module.hot.accept('./imports', () => render(Root))
+  ReactDOM.render(<Component />, root, onPostRender)
 }
 
 render(Root)
--- .docz/app/root.jsx
+++ .docz/app/root.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { hot } from 'react-hot-loader'
 import Theme from 'docz-theme-default'
+
+import { imports } from './imports'
 import db from './db.json'
 
-const Root = ({ imports }) => (
-  <Theme db={db} imports={imports} hashRouter={true} />
-)
+const Root = () => <Theme db={db} imports={imports} hashRouter={true} />
 
 export default hot(module)(Root)

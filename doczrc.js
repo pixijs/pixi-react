06f94502a005c93cdd7f2ef1593c699953c84088
Patrick Brouwer <patrick@inlet.nl>
2019-06-26 23:00:45 +0200

    Update docz

--- doczrc.js
+++ doczrc.js
@@ -1,10 +1,23 @@
 export default {
-  title: 'React Pixi',
-  description: 'Write PIXI applications using React declarative style',
+  title: 'React PIXI',
+  description: 'Write PIXI apps using React declarative style',
   propsParser: false,
   ordering: 'ascending',
-  hashRouter: true,
   dest: 'docs',
+  htmlContext: {
+    head: {
+      links: [
+        {
+          rel: 'stylesheet',
+          href: 'https://codemirror.net/theme/lucario.css',
+        },
+      ],
+    },
+  },
   themeConfig: {
+    mode: 'dark',
+    showPlaygroundEditor: true,
+    // codemirrorTheme: 'duotone-dark',
+    codemirrorTheme: 'lucario',
   },
 }

ccf7cd54d9a83ffb6863b76788522c2b1decd4f6
Paolo Tozzo <ptozzo@theworkshop.com>
2019-10-17 11:11:12 +0200

    fix issue with updating animated sprite texture

--- src/components/AnimatedSprite.js
+++ src/components/AnimatedSprite.js
@@ -3,19 +3,25 @@ import { getTextureFromProps, applyDefaultProps } from '../utils/props'
 
 const AnimatedSprite = (root, props) => {
   const { textures, images, autoUpdate, isPlaying = true, initialFrame } = props
+  const makeTexture = (textures) => {
+    return textures.map(texture => {
+      return getTextureFromProps('AnimatedSprite', {
+        texture,
+      })
+    })
+  }
   const animatedSprite = images
     ? PIXI.AnimatedSprite.fromImages(images)
-    : new PIXI.AnimatedSprite(
-        textures.map(texture => {
-          return getTextureFromProps('AnimatedSprite', {
-            texture,
-          })
-        })
-      )
+    : new PIXI.AnimatedSprite(makeTexture(textures))
   animatedSprite[isPlaying ? 'gotoAndPlay' : 'gotoAndStop'](initialFrame || 0)
   animatedSprite.applyProps = (instance, oldProps, newProps) => {
     const { textures, ...props } = newProps
+    const { isPlaying, initialFrame } = props
     applyDefaultProps(instance, oldProps, props)
+    if(textures) {
+      instance.textures = makeTexture(textures)
+      animatedSprite[isPlaying ? 'gotoAndPlay' : 'gotoAndStop'](initialFrame || 0)
+    }
   }
 
   return animatedSprite

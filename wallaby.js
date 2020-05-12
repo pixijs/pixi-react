5d4369a354ff5ea3b26d53820da30f5040e337ac
Patrick Brouwer <patrick@inlet.nl>
2020-05-12 10:28:29 +0200

    Clean up

--- wallaby.js
+++ wallaby.js
@@ -1,4 +1,4 @@
-module.exports = function(wallaby) {
+module.exports = function (wallaby) {
   return {
     files: ['src/**/*.js', 'package.json', 'test/__fixtures__/**/*', 'test/__utils__/**/*', 'test/bootstrap.js'],
     tests: ['test/**/*.js', '!test/bootstrap.js', '!test/__fixtures__/**/*', '!test/__utils__/**/*'],

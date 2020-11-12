011bc543002b57081ccccaf20683c57a20bd7bca
Patrick Brouwer <patrick@inlet.nl>
2020-11-12 10:42:37 +0100

    Add invariant to `useApp`

--- src/hooks/useApp.js
+++ src/hooks/useApp.js
@@ -1,6 +1,17 @@
+import invariant from '../utils/invariant'
 import { useContext } from 'react'
+import { Application } from 'pixi.js'
 import { Context } from '../stage/provider'
 
 export function useApp() {
-  return useContext(Context)
+  const app = useContext(Context)
+
+  invariant(
+    app instanceof Application,
+    'No Context found with `%s`. Make sure to wrap component with `%s`',
+    'PIXI.Application',
+    'AppProvider'
+  )
+
+  return app
 }

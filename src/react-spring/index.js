f7cd9e1dcc87adeca9f334ea851b6cbc515cc063
Patrick Brouwer <patrick@inlet.nl>
2021-06-04 11:26:21 +0200

    Apply default props when `applyProps` is not defined

--- src/react-spring/index.js
+++ src/react-spring/index.js
@@ -1,14 +1,16 @@
-import { createHost } from '@react-spring/animated'
+import spring from '@react-spring/animated'
 import { TYPES } from '../utils/element'
+import { applyDefaultProps } from '../utils/props'
 
 const primitives = Object.keys(TYPES)
 
-const host = createHost(primitives, {
+const host = spring.createHost(primitives, {
   applyAnimatedValues(instance, props) {
     if (!(instance.nodeType || instance.pluginName)) {
       return false
     }
-    instance.applyProps(instance, {}, props)
+    const applyProps = typeof instance?.applyProps === 'function' ? instance.applyProps : applyDefaultProps
+    applyProps(instance, {}, props)
   },
 })
 

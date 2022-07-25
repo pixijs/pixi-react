f82bee25e48da31f2c9c1f8b3f70f17adf10a463
Michael Bashurov <accounts@saitonakamura.me>
2022-07-25 17:48:23 +0300

    Fix events tests

--- test/events.spec.js
+++ test/events.spec.js
@@ -3,8 +3,8 @@ import * as PIXI from 'pixi.js'
 
 import hostconfig from '../src/reconciler/hostconfig'
 import { createElement } from '../src/utils/element'
-import { Text, Container, render, eventHandlers } from '../src'
-import { roots } from '../src/render'
+import { Text, Container, eventHandlers } from '../src'
+import { createRoot } from '../src/render'
 import { mockToSpy } from './__utils__/mock'
 
 const CUSTOM_EVENT = 'customEvent'
@@ -12,12 +12,15 @@ const NOT_CUSTOM_EVENT = 'notCustomEvent'
 
 jest.mock('../src/reconciler/hostconfig')
 
+const act = React.unstable_act
+
 describe('react', () => {
   let container = new PIXI.Container()
   container.root = true
+  let root
 
   // render in container
-  const renderInContainer = comp => render(comp, container)
+  const renderInContainer = comp => root.render(comp)
 
   // keep track of real PIXI instances created
   let instances = []
@@ -26,6 +29,7 @@ describe('react', () => {
     instances = []
     jest.clearAllMocks()
     mockToSpy('../src/reconciler/hostconfig')
+    root = createRoot(container)
 
     hostconfig.createInstance.mockImplementation((...args) => {
       const ins = createElement(...args)
@@ -35,7 +39,7 @@ describe('react', () => {
   })
 
   afterEach(() => {
-    roots.clear()
+    root.unmount()
     const index = eventHandlers.indexOf(CUSTOM_EVENT)
     if (index >= 0) eventHandlers.splice(index, 1)
   })
@@ -45,10 +49,12 @@ describe('react', () => {
       let onClick = jest.fn()
       let text
 
-      renderInContainer(
-        <Container>
-          <Text ref={c => (text = c)} click={onClick} />
-        </Container>
+      act(() =>
+        renderInContainer(
+          <Container>
+            <Text ref={c => (text = c)} click={onClick} />
+          </Container>
+        )
       )
 
       text.emit('click', { type: 'click', data: 123 })
@@ -62,9 +68,11 @@ describe('react', () => {
     test('dispose old event and assign new', () => {
       let onClick = jest.fn()
 
-      renderInContainer(<Text click={onClick} />)
-      renderInContainer(<Text click={onClick} />)
-      renderInContainer(<Text click={onClick} />)
+      act(() => {
+        renderInContainer(<Text click={onClick} />)
+        renderInContainer(<Text click={onClick} />)
+        renderInContainer(<Text click={onClick} />)
+      })
 
       instances[0].emit('click', { type: 'click', data: 123 })
 
@@ -80,7 +88,7 @@ describe('react', () => {
 
       eventHandlers.push(CUSTOM_EVENT)
 
-      renderInContainer(<Text ref={c => (text = c)} {...{ [CUSTOM_EVENT]: onCustomEvent }} />)
+      act(() => renderInContainer(<Text ref={c => (text = c)} {...{ [CUSTOM_EVENT]: onCustomEvent }} />))
 
       const customEvent = { type: CUSTOM_EVENT, data: 456 }
 
@@ -90,7 +98,7 @@ describe('react', () => {
       expect(onCustomEvent).toHaveBeenCalledTimes(1)
       expect(onCustomEvent).toHaveBeenCalledWith(customEvent)
 
-      renderInContainer(<Text ref={c => (text = c)} {...{ [NOT_CUSTOM_EVENT]: onNotCustomEvent }} />)
+      act(() => renderInContainer(<Text ref={c => (text = c)} {...{ [NOT_CUSTOM_EVENT]: onNotCustomEvent }} />))
 
       text.emit(NOT_CUSTOM_EVENT, { type: NOT_CUSTOM_EVENT, data: 789 })
 

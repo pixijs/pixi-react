a98a13817d45851ffa509fa755365d3c5938a02d
Michael Bashurov <accounts@saitonakamura.me>
2022-08-04 19:09:34 +0300

    Migrated more stories

--- src/components/SimpleMeshDocz.js
+++ src/components/SimpleMeshDocz.js
@@ -1,36 +1,19 @@
+import * as PIXI from 'pixi.js'
 /* eslint-disable */
 
 const w = 500
 const h = 300
 
-export const indices = new Uint16Array([
-  0,3,4,
-  0,1,4,
-  1,2,4,
-  2,4,5,
-  3,4,6,
-  4,6,7,
-  4,7,8,
-  4,5,8,
-])
+export const indices = new Uint16Array([0, 3, 4, 0, 1, 4, 1, 2, 4, 2, 4, 5, 3, 4, 6, 4, 6, 7, 4, 7, 8, 4, 5, 8])
 
-export const uvs = new Float32Array([
-  0, 0,          0.5, 0,          1, 0,
-  0, 0.5,        0.5, 0.5,        1, 0.5,
-  0, 1,          0.5, 1,          1, 1,
-])
+export const uvs = new Float32Array([0, 0, 0.5, 0, 1, 0, 0, 0.5, 0.5, 0.5, 1, 0.5, 0, 1, 0.5, 1, 1, 1])
 
-export const vertices = new Float32Array([
-  0,0,       w/2, 0,       w, 0,
-  0,h/2,     w/2, h/2,     w, h/2,
-  0,h,       w/2, h,       w, h,
-])
+export const vertices = new Float32Array([0, 0, w / 2, 0, w, 0, 0, h / 2, w / 2, h / 2, w, h / 2, 0, h, w / 2, h, w, h])
 
-let DRAW_MODES = 1;
+let DRAW_MODES = 1
 
 if (typeof window !== 'undefined') {
-  const PIXI = require('pixi.js');
-  DRAW_MODES = PIXI.DRAW_MODES;
+  DRAW_MODES = PIXI.DRAW_MODES
 }
 
 export { DRAW_MODES }

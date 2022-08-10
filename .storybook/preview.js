6b59a8ed6ef2402854b5e5681fd187a9ec674891
Michael Bashurov <accounts@saitonakamura.me>
2022-08-10 17:51:43 +0300

    Fixed order of the stories and added component to Meta

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,27 @@
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
+    },
+  },
+  options: {
+    storySort: {
+      order: [
+        'About',
+        'CDN Links',
+        'Components',
+        'Context Bridge',
+        'Custom Components',
+        'Fallback to Canvas',
+        'HOC',
+        'Hooks',
+        'React-Spring',
+        'Render',
+        'Stage',
+        'TypeScript',
+      ],
     },
   },
 }

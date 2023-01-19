33e6a716b944c586cb94b2423b406094560a3f52
Alex Prokop <alex.prokop@play.co>
2023-01-19 12:27:07 +0000

    move shared packages to root package.json for easier version management update pixi to v7

--- packages/react-pixi/src/reconciler/index.js
+++ packages/react-pixi/src/reconciler/index.js
@@ -1,5 +1,5 @@
 import Reconciler from 'react-reconciler';
-import pkg from '../../package.json';
+import pkg from '../../../../package.json';
 import hostconfig from './hostconfig';
 
 export const PixiFiber = Reconciler(hostconfig);

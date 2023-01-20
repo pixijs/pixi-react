e1b270853c7424af9273f5bdc1297174935444ca
Alex Prokop <alex.prokop@play.co>
2023-01-20 12:45:17 +0000

    change GH repo references

--- packages/docs/docusaurus.config.js
+++ packages/docs/docusaurus.config.js
@@ -17,7 +17,7 @@ const config = {
     // GitHub pages deployment config.
     // If you aren't using GitHub pages, you don't need these.
     organizationName: 'pixi', // Usually your GitHub org/user name.
-    projectName: 'react-pixi', // Usually your repo name.
+    projectName: 'pixi-react', // Usually your repo name.
 
     // Even if you don't use internalization, you can use this field to set useful
     // metadata like html lang. For example, if your site is Chinese, you may want

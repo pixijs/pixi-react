a3711dbbb7aa76dae3f4c4148020d7bdbdac4948
Igor <Hogar.fox@gmail.com>
2023-02-14 13:41:15 +0300

    Fix typings for sprite with children (#409)
    * Fix typings for sprite with children
    
    * Revert "Fix typings for sprite with children"
    
    This reverts commit 4f536c5660cc3a6a9f66b47b239192b24cddc5e9.
    
    * Fix typings for Sprite by using PropsWithChildren type
    
    * Fix typings for ParticleContainer by using PropsWithChildren type
    
    ---------
    
    Co-authored-by: Igor.korol <Igor.korol@stark.games>

--- packages/animated/index.d.ts
+++ packages/animated/index.d.ts
@@ -355,12 +355,12 @@ declare namespace _ReactPixi {
 
 // components
 export const Text: AnimatedComponent<React.FC<_ReactPixi.IText>>;
-export const Sprite: AnimatedComponent<React.FC<_ReactPixi.ISprite>>;
+export const Sprite: AnimatedComponent<React.FC<React.PropsWithChildren<_ReactPixi.ISprite>>>;
 export const Container: AnimatedComponent<React.FC<React.PropsWithChildren<_ReactPixi.IContainer>>>;
 export const Graphics: AnimatedComponent<React.FC<_ReactPixi.IGraphics>>;
 export const BitmapText: AnimatedComponent<React.FC<_ReactPixi.IBitmapText>>;
 export const NineSlicePlane: AnimatedComponent<React.FC<_ReactPixi.INineSlicePlane>>;
-export const ParticleContainer: AnimatedComponent<React.FC<_ReactPixi.IParticleContainer>>;
+export const ParticleContainer: AnimatedComponent<React.FC<React.PropsWithChildren<_ReactPixi.IParticleContainer>>>;
 export const TilingSprite: AnimatedComponent<React.FC<_ReactPixi.ITilingSprite>>;
 export const SimpleRope: AnimatedComponent<React.FC<_ReactPixi.ISimpleRope>>;
 export const SimpleMesh: AnimatedComponent<React.FC<_ReactPixi.ISimpleMesh>>;
--- packages/react/index.d.ts
+++ packages/react/index.d.ts
@@ -375,12 +375,12 @@ declare namespace _ReactPixi
 
 // components
 export const Text: React.FC<_ReactPixi.IText>;
-export const Sprite: React.FC<_ReactPixi.ISprite>;
+export const Sprite: React.FC<React.PropsWithChildren<_ReactPixi.ISprite>>;
 export const Container: React.FC<React.PropsWithChildren<_ReactPixi.IContainer>>;
 export const Graphics: React.FC<_ReactPixi.IGraphics>;
 export const BitmapText: React.FC<_ReactPixi.IBitmapText>;
 export const NineSlicePlane: React.FC<_ReactPixi.INineSlicePlane>;
-export const ParticleContainer: React.FC<_ReactPixi.IParticleContainer>;
+export const ParticleContainer: React.FC<React.PropsWithChildren<_ReactPixi.IParticleContainer>>;
 export const TilingSprite: React.FC<_ReactPixi.ITilingSprite>;
 export const SimpleRope: React.FC<_ReactPixi.ISimpleRope>;
 export const SimpleMesh: React.FC<_ReactPixi.ISimpleMesh>;

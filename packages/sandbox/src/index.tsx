e42c3b3b487721c6f1435f1b012bb297c09bb1e1
Alex Prokop <alex.prokop@play.co>
2023-03-02 11:49:28 +0000

    convert sandbox to TS

--- packages/sandbox/src/index.tsx
+++ packages/sandbox/src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root')!);
 
 root.render(
     <React.StrictMode>

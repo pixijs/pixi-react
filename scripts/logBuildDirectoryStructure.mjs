dae95f9b84032c3d77795a99732c641e25787a85
Alex Prokop <alex.prokop@play.co>
2023-03-03 15:25:09 +0000

    log built files after each package is built

---
+++ scripts/logBuildDirectoryStructure.mjs
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+import dirTree from 'directory-tree';
+import { readFileSync, writeFileSync } from 'fs';
+import { dirname, resolve } from 'path';
+import { fileURLToPath } from 'url';
+
+// utility to log out directory structure of build files beneath dist directories, to help debug issues on CI
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const packagesPath = resolve(__dirname, '..', 'packages');
+
+// annoyingly only supports exclude rather than include, so we have a fugly negative lookahead to only show us dist dirs
+const tree = dirTree(packagesPath, {
+    exclude: /(node_modules|docs|sandbox|test|src|scripts|react-fiber-react-18|react-components-pixi-7)/,
+    extensions: /\.(js|ts)$/
+});
+
+console.log(JSON.stringify(tree, null, 2));

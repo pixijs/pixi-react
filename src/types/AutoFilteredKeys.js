3ee6ebb575c1d4d040656ae676583b06c5cd9a26
Trezy <tre@trezy.com>
2024-05-28 10:02:21 -0500

    refactor: convert types to JSdoc

---
+++ src/types/AutoFilteredKeys.js
@@ -0,0 +1,13 @@
+/** @typedef {typeof import('pixi.js')} PixiType */
+
+/**
+ * @typedef {{
+ *    [K in keyof PixiType]: K extends import('./TargetKeys.js').TargetKeys
+ *      ? PixiType[K] extends new (...args: any) => any
+ *      ? K
+ *        : never
+ *      : never;
+ *  }[keyof PixiType]
+ * } AutoFilteredKeys
+ */
+export const AutoFilteredKeys = {};
---
+++ src/types/ConstructorParams.js
@@ -0,0 +1,10 @@
+/**
+ * @template {new (...args: any[]) => any} T
+ * @typedef {T extends new (args: infer A) => any ? A : never} ConstructorWithOneParam
+ */
+
+/**
+ * @template {new (...args: any[]) => any} T
+ * @typedef {ConstructorWithOneParam<T>} ConstructorParams
+ */
+export const ConstructorParams = {};

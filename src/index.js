16ad07a105a157efe7a96695e17df4a69effc477
Trezy <tre@trezy.com>
2024-06-03 21:57:06 -0500

    feat: export the vanilla `extend` method

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+export { extend } from './helpers/extend.js';
 export { useAsset } from './hooks/useAsset.js';
 export { useExtend } from './hooks/useExtend.js';
 export { render } from './render.js';

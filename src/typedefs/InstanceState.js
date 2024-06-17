e0cf4a7b4563e66a989981e758751a610871d49a
Trezy <tre@trezy.com>
2024-06-17 14:31:36 -0500

    feat: add support for pascal-cased events

--- src/typedefs/InstanceState.js
+++ src/typedefs/InstanceState.js
@@ -1,11 +1,8 @@
-/** @typedef {import('./EventHandlers.js').EventHandlers} EventHandlers */
 /** @typedef {import('./Instance.js').Instance} Instance */
 
 /**
  * @typedef {object} InstanceState
  * @property {boolean} [autoRemovedBeforeAppend]
- * @property {number} eventCount
- * @property {Partial<EventHandlers>} handlers
  * @property {null | Instance} parent
  * @property {Instance} root
  * @property {string} type

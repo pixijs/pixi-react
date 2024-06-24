fac4bb7acb0115e4c10808ce601f293def3e6051
Trezy <tre@trezy.com>
2024-06-24 12:06:59 -0500

    refactor: update `useAsset` for Suspense support (and add `useAssets`)

--- src/hooks/useAsset.js
+++ src/hooks/useAsset.js
@@ -1,65 +1,31 @@
-import { Assets } from 'pixi.js';
 import {
-    useEffect,
-    useState,
-} from 'react';
-import { isEqual } from '../helpers/compare.js';
+    Assets,
+    Cache,
+} from 'pixi.js';
+import { getAssetKeyFromOptions } from '../helpers/getAssetKeyFromOptions.js';
 
+/** @typedef {import('pixi.js').ProgressCallback} ProgressCallback */
 /**
- * @typedef {import('pixi.js').ProgressCallback} ProgressCallback
- * @typedef {import('pixi.js').Texture} Texture
- * @typedef {import('pixi.js').UnresolvedAsset} UnresolvedAsset
+ * @template T
+ * @typedef {import('pixi.js').UnresolvedAsset<T>} UnresolvedAsset
  */
 
 /**
  * Loads assets, returning a hash of assets once they're loaded.
  *
- * @param {UnresolvedAsset} options Asset options.
+ * @template T
+ * @param {UnresolvedAsset<T> | string} options Asset options.
  * @param {ProgressCallback} [onProgress] A function to be called when the asset loader reports loading progress.
- * @returns {null | Texture} A hash of textures, keyed by their URLs. This will be `null` until the assets are loaded.
+ * @returns {T}
  */
 export function useAsset(options, onProgress)
 {
-    const [isLoaded, setIsLoaded] = useState(false);
-    const [isLoading, setIsLoading] = useState(false);
-    const [memoizedOptions, setMemoizedOptions] = useState(options);
-    const [texture, setTexture] = useState(null);
+    const assetKey = getAssetKeyFromOptions(options);
 
-    useEffect(() =>
+    if (!Cache.has(assetKey))
     {
-        if (isEqual(options, memoizedOptions))
-        {
-            setMemoizedOptions(options);
-            setIsLoaded(false);
-        }
-    }, [
-        memoizedOptions,
-        options,
-    ]);
+        throw Assets.load(options, onProgress);
+    }
 
-    useEffect(() =>
-    {
-        if (!isLoaded && !isLoading)
-        {
-            setIsLoading(true);
-
-            const assetKey = /** @type {string} */ (options.alias ?? options.src);
-
-            Assets.add(options);
-            Assets
-                .load(assetKey, onProgress)
-                .then((texture) =>
-                {
-                    setTexture(texture);
-                    setIsLoaded(true);
-                    setIsLoading(false);
-                })
-                .catch(() => setIsLoading(false));
-        }
-    }, [
-        isLoaded,
-        isLoading,
-    ]);
-
-    return texture;
+    return Assets.get(assetKey);
 }

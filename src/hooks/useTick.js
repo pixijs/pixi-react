17e77d8a0b438ca740ab432cd1c813d246ce8fbd
Trezy <tre@trezy.com>
2024-07-04 23:22:04 -0500

    refactor: make `useTick` more resilient
    This update ensures that he callback passed to `useTick` will always be
    executed appropriately. This fixes a potential situation where the
    callback would be removed but not re-added because the value of
    `enabled` hadn't changed.
    
    Co-authored-by: Raymond <lunarraid@gmail.com>

--- src/hooks/useTick.js
+++ src/hooks/useTick.js
@@ -1,34 +1,14 @@
-import {
-    useEffect,
-    useRef,
-} from 'react';
+import { useEffect } from 'react';
 import { invariant } from '../helpers/invariant.js';
 import { useApp } from './useApp.js';
 
-/** @typedef {number} UPDATE_PRIORITY */
 /**
  * @template T
  * @typedef {import('pixi.js').TickerCallback<T>} TickerCallback
  */
 /**
  * @template T
- * @typedef {import('react').MutableRefObject<T>} MutableRefObject
- */
-
-/**
- * @template T
- * @typedef {object} TickCallbackOptions
- * @property {TickerCallback<T>} callback The function to be called on each tick.
- * @property {T} [context] The value of `this` within the callback.
- * @property {UPDATE_PRIORITY} [priority] The priority of this callback compared to other callbacks on the ticker.
- */
-
-/**
- * @template T
- * @typedef {object} TickState
- * @property {TickerCallback<T>} callback
- * @property {*} context
- * @property {number | undefined} priority
+ * @typedef {import('../typedefs/TickCallbackOptions.ts').TickCallbackOptions<T>} TickCallbackOptions
  */
 
 /**
@@ -63,50 +43,33 @@ function useTick(options, enabled = true)
 
     invariant(typeof callback === 'function', '`useTick` needs a callback function.');
 
-    /** @type {MutableRefObject<TickState<T>>} */
-    const stateRef = useRef({
-        callback,
-        context,
-        priority,
-    });
-
-    useEffect(() =>
-    {
-        if (app.ticker)
-        {
-            app.ticker.remove(stateRef.current.callback, stateRef.current.context);
-        }
-        stateRef.current.callback = callback;
-        stateRef.current.context = context;
-        stateRef.current.priority = priority;
-    }, [
-        callback,
-        context,
-        priority,
-    ]);
-
     // eslint-disable-next-line consistent-return
     useEffect(() =>
     {
-        if (enabled)
-        {
-            const {
-                callback: currentCallback,
-                context: currentContext,
-                priority: currentPriority,
-            } = stateRef.current;
+        const ticker = app?.ticker;
 
-            app.ticker.add(currentCallback, currentContext, currentPriority);
+        if (ticker)
+        {
+            const wasEnabled = enabled;
+            const previousContext = context;
+            const previousCallback = callback;
+
+            if (enabled && ticker)
+            {
+                ticker.add(callback, context, priority);
+            }
 
             return () =>
             {
-                if (app.ticker)
-                {
-                    app.ticker.remove(currentCallback, currentContext);
-                }
+                wasEnabled && ticker?.remove(previousCallback, previousContext);
             };
         }
-    }, [enabled]);
+    }, [
+        callback,
+        context,
+        enabled,
+        priority,
+    ]);
 }
 
 export { useTick };

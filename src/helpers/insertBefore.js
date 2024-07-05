d66cfc8ca73d32d59cb2cb971242fbcf327e2fd8
Trezy <tre@trezy.com>
2024-07-04 22:58:52 -0500

    fix: remove false positive invariant when changing a component's children
    fixes #493

--- src/helpers/insertBefore.js
+++ src/helpers/insertBefore.js
@@ -12,7 +12,7 @@ export function insertBefore(parentInstance, childInstance, beforeChildInstance)
 {
     log('info', 'lifecycle::insertBefore');
 
-    invariant(childInstance === beforeChildInstance, 'Cannot insert node before itself');
+    invariant(childInstance !== beforeChildInstance, 'Cannot insert node before itself');
 
     if (parentInstance.children.indexOf(childInstance) === -1)
     {

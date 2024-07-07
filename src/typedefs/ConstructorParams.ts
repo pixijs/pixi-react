852140bac248c4048b0f392f2e6e686cf27a5f0c
Trezy <tre@trezy.com>
2024-07-06 22:47:07 -0500

    fix: make sure `<text>` components have the correct props

--- src/typedefs/ConstructorParams.ts
+++ src/typedefs/ConstructorParams.ts
@@ -1,1 +1,15 @@
-export type ConstructorParams<T extends abstract new (...args: any) => any> = T extends new (...args: infer A) => any ? A[0] : never;
+import type {
+    Text,
+    TextOptions,
+} from 'pixi.js';
+
+export type ConstructorParams<T extends abstract new (...args: any) => any> =
+    /**
+     * We're adding a specific options type override for Text components because of the order of overloads.
+     * @see https://github.com/pixijs/pixi-react/issues/500
+     */
+    T extends typeof Text
+        ? TextOptions
+        : T extends new (...args: infer A) => any
+            ? A[0]
+            : never;

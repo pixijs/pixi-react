acd26b0e91ca29044a457228b72a1562607def54
Trezy <tre@trezy.com>
2024-07-11 10:28:36 -0500

    refactor: add better support for constructor overrides
    blep

--- src/typedefs/PixiReactNode.ts
+++ src/typedefs/PixiReactNode.ts
@@ -1,5 +1,6 @@
-import type { ConstructorParams } from './ConstructorParams.ts';
+import type { ConstructorOptions } from './ConstructorOptions.ts';
 import type { Overwrite } from './Overwrite.ts';
 import type { PixiReactNodeProps } from './PixiReactNodeProps.ts';
 
-export type PixiReactNode<T extends abstract new (...args: any) => any> = Overwrite<Partial<ConstructorParams<T>>, PixiReactNodeProps<InstanceType<T>>>;
+export type PixiReactNode<T extends abstract new (...args: any) => any> =
+    Overwrite<Partial<ConstructorOptions<T>>, PixiReactNodeProps<InstanceType<T>>>;

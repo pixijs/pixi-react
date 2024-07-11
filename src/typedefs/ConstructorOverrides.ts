533cf87eda8f23da5541dfdc8ff4db1ff522934f
Trezy <tre@trezy.com>
2024-07-11 10:29:00 -0500

    fix: add constructor overrides for filters

--- src/typedefs/ConstructorOverrides.ts
+++ src/typedefs/ConstructorOverrides.ts
@@ -1,7 +1,25 @@
 import type {
+    AlphaFilter,
+    AlphaFilterOptions,
+    BlurFilter,
+    BlurFilterOptions,
+    BlurFilterPass,
+    BlurFilterPassOptions,
+    DisplacementFilter,
+    DisplacementFilterOptions,
+    Filter,
+    FilterOptions,
+    NoiseFilter,
+    NoiseFilterOptions,
     Text,
     TextOptions,
 } from 'pixi.js';
 
 export type ConstructorOverrides =
+    | [typeof AlphaFilter, AlphaFilterOptions]
+    | [typeof BlurFilter, BlurFilterOptions]
+    | [typeof BlurFilterPass, BlurFilterPassOptions]
+    | [typeof DisplacementFilter, DisplacementFilterOptions]
+    | [typeof Filter, FilterOptions]
+    | [typeof NoiseFilter, NoiseFilterOptions]
     | [typeof Text, TextOptions];

a253d3c9b3525a9f1c5284ef3ca3febf320afea3
Trezy <tre@trezy.com>
2024-07-12 11:33:20 -0500

    ci: disable npm publishing with semantic-release

--- release.config.js
+++ release.config.js
@@ -10,4 +10,10 @@ module.exports = {
             prerelease: true,
         },
     ],
+    plugins: [
+        '@semantic-release/commit-analyzer',
+        '@semantic-release/release-notes-generator',
+        // '@semantic-release/npm',
+        '@semantic-release/github',
+    ],
 };

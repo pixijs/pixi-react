7f066b0aef744a3a40767c6928c49efa31487a9e
Trezy <tre@trezy.com>
2024-07-12 12:22:23 -0500

    feat: add npm publishing to CI

--- release.config.js
+++ release.config.js
@@ -13,7 +13,12 @@ module.exports = {
     plugins: [
         '@semantic-release/commit-analyzer',
         '@semantic-release/release-notes-generator',
-        // '@semantic-release/npm',
-        '@semantic-release/github',
+        ['@semantic-release/npm', {
+            npmPublish: false,
+            tarballDir: 'dist',
+        }],
+        ['@semantic-release/github', {
+            assets: 'dist/*.tgz',
+        }],
     ],
 };

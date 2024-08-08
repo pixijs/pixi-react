26b969aa846814895d2ea8d8e82af064984f0a9a
Justin Walsh <contact.me@thejustinwalsh.com>
2024-08-08 09:53:31 -0400

    revert constructor options changes replace never with unknown for composition

--- test/unit/typedefs/PixiElements.test.tsx
+++ test/unit/typedefs/PixiElements.test.tsx
@@ -13,10 +13,11 @@ describe('PixiElements', () =>
                 <graphics draw={(_: Graphics) => { /* noop */ }} />
                 <sprite texture={Texture.EMPTY} />
                 <alphaFilter alpha={0.5} />
+                <pixiText anchor={{ x: 0.5, y: 0.5 }} text="Hello, World!" />
             </container>
         );
 
-        expect(elements.props.children).to.have.length(3);
+        expect(elements.props.children).to.have.length(4);
 
         expect(elements.props.children[0].type).to.equal('graphics');
         expect(elements.props.children[0].props.draw).to.be.a('function');
@@ -27,5 +28,8 @@ describe('PixiElements', () =>
 
         expect(elements.props.children[2].type).to.equal('alphaFilter');
         expect(elements.props.children[2].props.alpha).to.equal(0.5);
+
+        expect(elements.props.children[3].type).to.equal('pixiText');
+        expect(elements.props.children[3].props.text).to.equal('Hello, World!');
     });
 });

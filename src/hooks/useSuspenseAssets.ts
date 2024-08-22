800d9e52a39c743e778a126ca785493b423d5b17
Justin Walsh <contact.me@thejustinwalsh.com>
2024-08-21 23:43:09 -0400

    fix: useSuspenseAssets

--- src/hooks/useSuspenseAssets.ts
+++ src/hooks/useSuspenseAssets.ts
@@ -18,7 +18,7 @@ function assetsLoadedTest<T>(asset: UnresolvedAsset<T>)
 /** Loads assets, returning a hash of assets once they're loaded. Must be inside of a `<Suspense>` component. */
 export function useSuspenseAssets<T = any>(
     /** @description Assets to be loaded. */
-    assets: UnresolvedAsset<T>[],
+    assets: UnresolvedAsset[],
     /** @description Asset options. */
     options: UseAssetsOptions = {},
 ): T[]

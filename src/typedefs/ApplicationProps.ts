2e79fc088412b8deec3ed45299e5b028e0f8dc9b
Trezy <tre@trezy.com>
2024-08-26 16:07:44 +0200

    fix: match Pixi expectations for extensions

--- src/typedefs/ApplicationProps.ts
+++ src/typedefs/ApplicationProps.ts
@@ -26,7 +26,7 @@ export interface BaseApplicationProps
     defaultTextStyle?: TextStyle | TextStyleOptions,
 
     /** @description An array of Pixi extensions to be loaded before initialisation. */
-    extensions?: ExtensionFormatLoose[],
+    extensions?: (ExtensionFormatLoose | any)[],
 
     /** @description A unique key which allows React to manage this component across changes in parent state. */
     key?: Key,

f429cd6c52bf9e701b31ee77c3dbfad0d3b5b459
Trezy <tre@trezy.com>
2024-09-03 23:02:51 -0500

    chore: remove unused test util

--- test/utils/wait.ts
+++
@@ -1,7 +0,0 @@
-export function wait(waitMS: number, rejectOnComplete: boolean = false)
-{
-    return new Promise((resolve, reject) =>
-    {
-        setTimeout(rejectOnComplete ? reject : resolve, waitMS);
-    });
-}
